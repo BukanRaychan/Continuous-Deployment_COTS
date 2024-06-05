6b323135e04c2a354837d3609debeb548e96f400
BukanRaychan <fasyaraihan82@gmail.com>
2024-06-05 09:31:16 +0700

    init

---
+++ src/main/java/com/mycompany/tesrelease/Tesrelease.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tesrelease;
+
+/**
+ *
+ * @author Raychan
+ */
+public class Tesrelease {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
